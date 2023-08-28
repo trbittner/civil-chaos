985d80694740b63f0d5e6b2585b3a36556418f73
Todd Bittner <toddbittner@gmail.com>
2023-08-28 18:36:56 +0000

    Created the basic structure and styling for the blog.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				pacifico: ['Pacifico','cursive'],
+				poppins: ['Poppins','sans-serif'],
+				lora: ['Lora','serif'],					
+			},
+		},
 	},
 	plugins: [],
 }
